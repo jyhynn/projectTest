fd0c946e343ed910cc62275e8b3fe564c0d0cb1c
curatio <yhm0411@naver.com>
2019-03-27 11:15:19 +0900

    get set

--- project/src/project/BookVO.java
+++ project/src/project/BookVO.java
@@ -5,6 +5,30 @@ public class BookVO {
 	private String title;
 	private String author;
 	private int price;
+	public String getCode() {
+		return code;
+	}
+	public void setCode(String code) {
+		this.code = code;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
 	public BookVO() {
 		super();
 		// TODO Auto-generated constructor stub
